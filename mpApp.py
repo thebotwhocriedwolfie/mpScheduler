ea8cd2ce17561226fa5334fadb0daf06b6724cfa
Kiirthanah <167096354+thebotwhocriedwolfie@users.noreply.github.com>
2025-06-03 13:18:51 +0800

    Update mpApp.py

--- mpApp.py
+++ mpApp.py
@@ -30,5 +30,6 @@ def api_generate_schedule():
         if os.path.exists(file_path):
             os.remove(file_path)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
