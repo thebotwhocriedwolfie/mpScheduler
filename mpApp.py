9d8a31b8ca76a44ef184d29559eb36258b0ea8c5
Kiirthanah <167096354+thebotwhocriedwolfie@users.noreply.github.com>
2025-06-14 14:54:23 +0800

    Update mpApp.py to enable cors

--- mpApp.py
+++ mpApp.py
@@ -1,11 +1,12 @@
 from flask import Flask, request, jsonify
 from scheduler import generate_schedule  #import function from scheduler.py
-
+from flask_cors import CORS  #import cors
 
 app = Flask(__name__)
+CORS(app) #enable cors
 
-@app.route('/generate_schedule', methods=['POST'])
-def api_generate_schedule():
+@app.route('/generate_schedule', methods=['POST']) #post api route
+def api_generate_schedule(): #call the scheduler
     data = request.json
     file_path = data.get('file_path')
 
@@ -14,7 +15,7 @@ def api_generate_schedule():
 
     try:
         result = generate_schedule(file_path)
-        return jsonify(result)  # Returns {'schedule': [...]}
+        return jsonify(result)  # Returns schedule results
     
     except Exception as e:
         return jsonify({'error': str(e)}), 500
